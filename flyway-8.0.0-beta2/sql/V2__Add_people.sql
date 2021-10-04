02334c56fd982b77c9f90f4935d5c13cf355c512
Vijay Chenna <chennavijaykumar@gmail.com>
2021-10-04 20:04:21 +0530

    Change script for v1

--- flyway-8.0.0-beta2/sql/V2__Add_people.sql
+++ flyway-8.0.0-beta2/sql/V2__Add_people.sql
@@ -3,4 +3,4 @@ insert into PERSON (ID, NAME) values (2, 'Mr. Foo');
 insert into PERSON (ID, NAME) values (3, 'Ms. Bar');
 insert into PERSON (ID, NAME) values (4, 'John');
 insert into PERSON (ID, NAME) values (5, 'Samuel');
-insert into PERSON (ID, NAME) values (6, 'Prejay');
+insert into PERSON (ID, NAME) values (6, 'Prejay123');
